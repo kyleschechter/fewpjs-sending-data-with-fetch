10326f6588f4f40341e5bd56d4ec1a3bf2f93044
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-06-03 14:34:29 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+    const testOneUrl = 'http://localhost:3000/users'
+   
+    function submitData(name, email) {
+        return fetch(testOneUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                email
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => document.body.append(data.id)
+        )
+        .catch(error => document.body.append(error.message)
+        )
+    }
